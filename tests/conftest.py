a5f62e591d2a72546122ffb1b19289a640046e74
rafly <raflylesmana@xaltius.tech>
2024-02-20 16:38:05 +0700

    add: unit test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,20 +4,7 @@ import os
 
 
 @pytest.fixture
-def test_app():
-      app.config['TESTING'] = True
-
-      # Get database configuration from environment variables
-      DATABASE_TYPE = os.getenv('DATABASE_TYPE')
-      DATABASE_USER = os.getenv('DATABASE_USER')
-      DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
-      DATABASE_HOST = os.getenv('DATABASE_HOST')
-      DATABASE_NAME = os.getenv('DATABASE_NAME')
-      DATABASE_PORT = os.getenv('DATABASE_PORT')
-      
-      # Configure the database URI
-      app.config['SQLALCHEMY_DATABASE_URI'] = f"{DATABASE_TYPE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-
-
+def app():
+      test_app
       with app.test_client() as client:
             yield client
--- tests/controller/test_customer_controller.py
+++ tests/controller/test_customer_controller.py
@@ -1,20 +1,36 @@
 def test_get_customers(test_app):
    response =test_app.get("v1/customers/")
    print(response.json['data'])
-   assert len(response.json['data']) == 6
+   assert len(response.json['data']) == 7
 
 def test_post_customers(test_app):
    data = {
          "name": "raly", 
          "phone": 23,
+         "gender": "laki laki"
    }
+   expected_response = {
+      'id': 21, 
+      'name': 'raly', 
+      'phone': 23
+   }
+
    response = test_app.post("v1/customers/", json=data)
 
-   assert response.json == 201
+   assert response.json['data'] == expected_response
+   assert response.status_code == 201
+
+def test_put_customers(test_app):
+   data =  {
+         "name": "rafly", 
+         "phone": 23,
+   }
+   response = test_app.put("v1/customers/22", json=data)
+   
    assert response.status_code == 200
 
-def test_put_customers_400(test_app):
-   data = {}
-   response = test_app.put("v1/customers/1", json=data)
+
+def test_put_customers(test_app):
+   response = test_app.delete("v1/customers/22")
    
-   assert response.status_code == 400
+   assert response.status_code == 200
