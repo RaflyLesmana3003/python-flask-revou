b19ed11122ca3c69ba04f96eaac0335009e6ad2c
rafly <raflylesmana@xaltius.tech>
2024-02-21 21:01:19 +0700

    fix: update pytest conft

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from app.utils.database import db
 @pytest.fixture
 def test_app():
       """test application setup"""
-      app.config['FLASK_ENV'] = 'testing'
+      app.config['TESTING'] = True
+      app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:"
       with app.app_context():
         yield app
